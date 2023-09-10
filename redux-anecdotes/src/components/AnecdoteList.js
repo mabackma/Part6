e46e61d23ad42b5095a643ec9da11ebd2c1351a6
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2023-09-10 03:23:24 +0300

    filtering anecdotes

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,12 @@ import { useSelector, useDispatch } from "react-redux"
 import { addVote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    if ( state.filter === 'ALL' ) {
+      return state.anecdotes
+    }
+    return state.anecdotes.filter(anecdote => anecdote.content.includes(state.filter))
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
