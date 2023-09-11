4af8cd699c9caf44eafe0821c203f55235299429
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2023-09-11 04:08:59 +0300

    json-server

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -2,8 +2,26 @@ import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import store from './store'
+
+anecdoteService.getAll().then(anecdotes =>
+  store.dispatch(setAnecdotes(anecdotes))
+)
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => {
+          console.log('anecdotes!!: ', anecdotes)
+          dispatch(setAnecdotes(anecdotes))
+        })
+  }, [])
+
   return (
     <div>
       <Notification />
